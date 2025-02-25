2b7c1b5ecd2b8421b12f264fb4c10ed7016b25c3
kassyade <benjamin.aviles.std@gmail.com>
2025-02-25 12:14:09 +0100

    x

--- StudentTrack/src/servicios/ServicioUsuario.js
+++
@@ -1,9 +0,0 @@
-import http from "./http-axios.js";
-
-class ServicioUsuario {
-  login(usuario, pass) {
-    return http.get(`/usuarios?nombre=${usuario}&pass=${pass}`);
-  }
-}
-
-export default new ServicioUsuario();
